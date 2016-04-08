2b7b21d8380743a560325569b3c91f021ec36a64
Benjamin Kostreva <benkostr@gmail.com>
2016-04-07 19:08:18 -0600

    Implemented GUI in WPF
    Changed class to Sorter.cs and made it compile to a .dll
    ImageSort is the WPF UI.
    
    This is extremely inefficient... The garbage collector does
    not delete the stored information for a long time... This
    program (Release build) was using 1.7 GB of RAM at one point,
    since I ran it 3 times in a row. It didn't garbage collect
    until after that, at which point it was only using 11 KB.
    
    It probably has to do with my code, but I'm going to write this
    in C++ with QT (with the intent to make it cross-platform), and
    I will be able to manually manage memory.

---
+++ ImageSort/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageSort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_start_Click(object sender, RoutedEventArgs e)
+        {
+            // Read settings, initialize Sorter, Sort
+            if (!Directory.Exists(this.textBox_sourceDir.Text))
+            {
+                System.Windows.MessageBox.Show(this, "Error: Cannot find source path:\n" + this.textBox_sourceDir.Text);
+                return;
+            }
+            else if (!Directory.Exists(this.textBox_targetDir.Text))
+            {
+                System.Windows.MessageBox.Show(this, "Error: Cannot find target path:\n" + this.textBox_targetDir.Text);
+                return;
+            }
+            else
+            {
+                Sorter.Sorter sorter = new Sorter
+                    .Sorter(this.textBox_sourceDir.Text, this.textBox_targetDir.Text)
+                {
+                    sortByYear = this.checkBox_year.IsChecked ?? true,
+                    sortByMonth = this.checkBox_month.IsChecked ?? true,
+                    sortByDay = this.checkBox_day.IsChecked ?? false,
+                    sortByHour = this.checkBox_hour.IsChecked ?? false,
+                    sortByMinute = this.checkBox_minute.IsChecked ?? false,
+                    sortBySecond = this.checkBox_second.IsChecked ?? false,
+                    sortNonExif = this.checkBox_sortNonExif.IsChecked ?? true,
+                    separateNonExif = this.checkBox_separateNonExif.IsChecked ?? true,
+                    move = this.radioButton_move.IsChecked ?? false
+                };
+
+                sorter.Sort();
+            }
+        }
+
+        private void button_chooseSourceDir_Click(object sender, RoutedEventArgs e)
+        {
+            // Open directory prompt, paste into textbox
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox_sourceDir.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button_chooseTargetDir_Click(object sender, RoutedEventArgs e)
+        {
+            // Open directory prompt, paste into textbox
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox_targetDir.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void checkBox_day_Click(object sender, RoutedEventArgs e)
+        {
+            // Enable/disable hour, minute, second checkboxes
+            if (checkBox_hour.IsEnabled == false)
+            {
+                checkBox_hour.IsEnabled = true;
+                checkBox_minute.IsEnabled = true;
+                checkBox_second.IsEnabled = true;
+            }
+            else if (checkBox_hour.IsEnabled == true)
+            {
+                checkBox_hour.IsChecked = false;
+                checkBox_minute.IsChecked = false;
+                checkBox_second.IsChecked = false;
+                checkBox_hour.IsEnabled = false;
+                checkBox_minute.IsEnabled = false;
+                checkBox_second.IsEnabled = false;
+            }
+
+        }
+
+        private void radioButton_move_Click(object sender, RoutedEventArgs e)
+        {
+            // Uncheck copy radiobutton
+            radioButton_copy.IsChecked = false;
+        }
+
+        private void radioButton_copy_Click(object sender, RoutedEventArgs e)
+        {
+            // Uncheck move radiobutton
+            radioButton_move.IsChecked = false;
+        }
+
+        private void checkBox_sortNonExif_Click(object sender, RoutedEventArgs e)
+        {
+            // Enable separateNonExif checkbox
+            if (checkBox_separateNonExif.IsEnabled == false)
+            {
+                checkBox_separateNonExif.IsEnabled = true;
+            }
+            else if (checkBox_separateNonExif.IsEnabled == true)
+            {
+                checkBox_separateNonExif.IsChecked = false;
+                checkBox_separateNonExif.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ Sorter/Sorter.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Globalization;
+
+namespace Sorter
+{
+    public class Sorter
+    {
+        public bool move { get; set; }
+        public bool sortNonExif { get; set; }
+        public bool separateNonExif { get; set; }
+        public bool sortByYear { get; set; }
+        public bool sortByMonth { get; set; }
+        public bool sortByDay { get; set; }
+        public bool sortByHour { get; set; }
+        public bool sortByMinute { get; set; }
+        public bool sortBySecond { get; set; }
+        public bool abbreviateMonths { get; set; }
+        public string sourcePath { get; set; }
+        public string outDir { get; set; }
+
+        private DateTime dt;
+        private DateTime defaultDT = default(DateTime);
+        private IEnumerable<string> files;
+        private string fileName, sourceFile;
+        private string month, day;
+        private string targetPath, destFile;
+
+        public Sorter(string sourcePath, string outDir)
+        {
+            this.move = false;
+            this.sortNonExif = true;
+            this.separateNonExif = true;
+            this.sortByYear = true;
+            this.sortByMonth = true;
+            this.sortByDay = true;
+            this.sortByHour = false;
+            this.sortByMinute = false;
+            this.sortBySecond = false;
+            this.abbreviateMonths = true;
+            this.sourcePath = sourcePath;
+            this.outDir = outDir;
+        }
+
+        public void Sort()
+        {
+            files = Directory
+                .EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                .Where(file =>
+                    file.ToLower().EndsWith("jpg") ||
+                    file.ToLower().EndsWith("jpeg") ||
+                    file.ToLower().EndsWith("png") ||
+                    file.ToLower().EndsWith("bmp") ||
+                    file.ToLower().EndsWith("dib")
+                    )
+                .ToList();
+
+            foreach (string file in files)
+            {
+                fileName = file.Substring(sourcePath.Length + 1);
+                sourceFile = Path.Combine(sourcePath, fileName);
+                targetPath = outDir;
+                dt = DateTaken(sourceFile) ?? defaultDT;
+                if (dt.Equals(defaultDT))
+                {
+                    if (!sortNonExif)
+                    {
+                        continue;
+                    }
+                    else if (separateNonExif)
+                    {
+                        targetPath = Path.Combine(targetPath, "NonEXIF");
+                        dt = File.GetLastWriteTime(sourceFile);
+                    }
+                }
+
+                if (sortByYear)
+                {
+                    targetPath = Path.Combine(targetPath, dt.Year.ToString());
+                }
+                if (sortByMonth)
+                {
+                    if (abbreviateMonths)
+                    {
+                        month = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(dt.Month);
+                    }
+                    else
+                    {
+                        month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dt.Month);
+                    }
+                    targetPath = Path.Combine(targetPath, month);
+                }
+                if (sortByDay)
+                {
+                    if (sortByMonth) { day = dt.Day.ToString(); }
+                    else { day = dt.DayOfYear.ToString(); }
+                    targetPath = Path.Combine(targetPath, day);
+                }
+                if (sortByHour)
+                {
+                    targetPath = Path.Combine(targetPath, dt.Hour.ToString());
+                }
+                if (sortByMinute)
+                {
+                    targetPath = Path.Combine(targetPath, dt.Day.ToString());
+                }
+                if (sortBySecond)
+                {
+                    targetPath = Path.Combine(targetPath, dt.Second.ToString());
+                }
+                Directory.CreateDirectory(targetPath);
+                destFile = Path.Combine(targetPath, fileName);
+
+                if (move)
+                {
+                    if (File.Exists(destFile)) { File.Delete(destFile); }
+                    File.Move(sourceFile, destFile);
+                }
+                else { File.Copy(sourceFile, destFile, true); }
+            }
+        }
+
+        private static DateTime? DateTaken(string imagePath)
+        {
+            Image getImage = Image.FromFile(imagePath);
+            int DateTakenValue = 0x9003; //36867;
+
+            if (!getImage.PropertyIdList.Contains(DateTakenValue))
+                return null;
+
+            string dateTakenTag = Encoding.ASCII.GetString(getImage.GetPropertyItem(DateTakenValue).Value);
+            if (dateTakenTag.Contains("."))
+            {
+                return Convert.ToDateTime(dateTakenTag);
+            }
+            else
+            {
+                string[] parts = dateTakenTag.Split(':', ' ');
+                int year = int.Parse(parts[0]);
+                int month = int.Parse(parts[1]);
+                int day = int.Parse(parts[2]);
+                int hour = int.Parse(parts[3]);
+                int minute = int.Parse(parts[4]);
+                int second = int.Parse(parts[5]);
+
+                return new DateTime(year, month, day, hour, minute, second);
+            }
+        }
+    }
+}
