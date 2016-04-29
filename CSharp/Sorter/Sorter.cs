210b1718f56d0572d1c72b598cbbae3daf271d80
Benjamin Kostreva <benkostr@gmail.com>
2016-04-29 03:23:52 -0600

    Implement multi-threading
    Creates as many threads as logical processors and separates the files
    evenly to be sorted by each thread.

--- CSharp/Sorter/Sorter.cs
+++ CSharp/Sorter/Sorter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Drawing;
 using System.Globalization;
 
@@ -23,12 +24,11 @@ namespace Sorter
         public string sourcePath { get; set; }
         public string outDir { get; set; }
 
-        private DateTime dt;
-        private DateTime defaultDT = default(DateTime);
+        private List<string>[] partitionedFiles;
+        private List<Thread> threads;
+        private int numProcs = Environment.ProcessorCount;
+        private int numThreads;
         private IEnumerable<string> files;
-        private string fileName, sourceFile;
-        private string month, day;
-        private string targetPath, destFile;
 
         public Sorter(string sourcePath, string outDir)
         {
@@ -46,7 +46,7 @@ namespace Sorter
             this.outDir = outDir;
         }
 
-        public void Sort()
+        public void Start()
         {
             files = Directory
                 .EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
@@ -59,7 +59,84 @@ namespace Sorter
                     )
                 .ToList();
 
-            foreach (string file in files)
+            // Number of threads equals number of logical processors unless
+            // there are less files than logical processes.
+            numThreads = (numProcs <= files.Count()) ? numProcs : files.Count();
+            partitionedFiles = PartitionList(files.ToList<string>(), numThreads);
+
+            threads = new List<Thread>();
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads.Add(new Thread(new ParameterizedThreadStart(Sort)));
+            }
+            for (int i = 0; i < numThreads; i++) { threads[i].Start(partitionedFiles[i]); }
+            for (int i = 0; i < numThreads; i++) { threads[i].Join(); }
+            threads.Clear();
+        }
+
+        public static List<T>[] PartitionList<T>(List<T> list, int totalPartitions)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            if (totalPartitions < 1)
+                throw new ArgumentOutOfRangeException("totalPartitions");
+
+            List<T>[] partitions = new List<T>[totalPartitions];
+
+            int maxSize = (int)Math.Ceiling(list.Count / (double)totalPartitions);
+            int k = 0;
+
+            for (int i = 0; i < partitions.Length; i++)
+            {
+                partitions[i] = new List<T>();
+                for (int j = k; j < k + maxSize; j++)
+                {
+                    if (j >= list.Count)
+                        break;
+                    partitions[i].Add(list[j]);
+                }
+                k += maxSize;
+            }
+
+            return partitions;
+        }
+
+        private static DateTime? DateTaken(string imagePath)
+        {
+            Image getImage = Image.FromFile(imagePath);
+            int DateTakenValue = 0x9003; //36867;
+
+            if (!getImage.PropertyIdList.Contains(DateTakenValue))
+                return null;
+
+            string dateTakenTag = Encoding.ASCII.GetString(getImage.GetPropertyItem(DateTakenValue).Value);
+            if (dateTakenTag.Contains("."))
+            {
+                return Convert.ToDateTime(dateTakenTag);
+            }
+            else
+            {
+                string[] parts = dateTakenTag.Split(':', ' ');
+                int year = int.Parse(parts[0]);
+                int month = int.Parse(parts[1]);
+                int day = int.Parse(parts[2]);
+                int hour = int.Parse(parts[3]);
+                int minute = int.Parse(parts[4]);
+                int second = int.Parse(parts[5]);
+
+                return new DateTime(year, month, day, hour, minute, second);
+            }
+        }
+
+        private void Sort(object imageFiles)
+        {
+            DateTime dt;
+            DateTime defaultDT = default(DateTime);
+            string fileName, sourceFile;
+            string month, day;
+            string targetPath, destFile;
+            foreach (string file in (List<string>)imageFiles)
             {
                 fileName = file.Substring(sourcePath.Length + 1);
                 sourceFile = Path.Combine(sourcePath, fileName);
@@ -121,34 +198,8 @@ namespace Sorter
                     File.Move(sourceFile, destFile);
                 }
                 else { File.Copy(sourceFile, destFile, true); }
-                GC.Collect();
-            }
-        }
-
-        private static DateTime? DateTaken(string imagePath)
-        {
-            Image getImage = Image.FromFile(imagePath);
-            int DateTakenValue = 0x9003; //36867;
-
-            if (!getImage.PropertyIdList.Contains(DateTakenValue))
-                return null;
-
-            string dateTakenTag = Encoding.ASCII.GetString(getImage.GetPropertyItem(DateTakenValue).Value);
-            if (dateTakenTag.Contains("."))
-            {
-                return Convert.ToDateTime(dateTakenTag);
-            }
-            else
-            {
-                string[] parts = dateTakenTag.Split(':', ' ');
-                int year = int.Parse(parts[0]);
-                int month = int.Parse(parts[1]);
-                int day = int.Parse(parts[2]);
-                int hour = int.Parse(parts[3]);
-                int minute = int.Parse(parts[4]);
-                int second = int.Parse(parts[5]);
-
-                return new DateTime(year, month, day, hour, minute, second);
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+                GC.WaitForPendingFinalizers();
             }
         }
     }
